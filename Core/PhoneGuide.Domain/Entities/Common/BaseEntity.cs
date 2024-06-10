bfcda2e3c4f5a9f176ebee28126b76a2d6617bb7
Aykut DEMİRCİ <aykutdemirci@outlook.com>
2024-06-10 19:29:33 +0300

    dbContext created

--- Core/PhoneGuide.Domain/Entities/Common/BaseEntity.cs
+++ Core/PhoneGuide.Domain/Entities/Common/BaseEntity.cs
@@ -3,5 +3,9 @@
     public class BaseEntity
     {
         public Guid Id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
     }
 }
---
+++ Infrastructure/PhoneGuide.Persistance/Contexts/PhoneGuideDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneGuide.Domain.Entities;
+using PhoneGuide.Domain.Entities.Common;
+
+namespace PhoneGuide.Persistance.Contexts
+{
+    public class PhoneGuideDbContext : DbContext
+    {
+        public PhoneGuideDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker.Entries<BaseEntity>();
+
+            foreach (var entry in entries)
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreatedDate = DateTime.Now;
+                }
+                else if (entry.State == EntityState.Modified)
+                {
+                    entry.Entity.UpdatedDate = DateTime.Now;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public DbSet<Person> Persons { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
