0c4f98d48e000d1d67f49a52e960ea7590beff1c
Aykut DEMİRCİ <aykutdemirci@outlook.com>
2024-06-11 01:12:51 +0300

    repository added

---
+++ Core/PhoneGuide.Application/Repositories/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using PhoneGuide.Domain.Entities.Common;
+using System.Linq.Expressions;
+
+namespace PhoneGuide.Application.Repositories
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<bool> AddAsync(T entity);
+
+        Task<bool> AddRangeAsync(List<T> entites);
+
+        Task<bool> UpdateAsync(T entity);
+
+        Task<bool> DeleteAsync(T entity);
+
+        Task<bool> DeleteByIdAsync(Guid id);
+
+        Task<bool> DeleteRangeAsync(List<T> entities);
+
+        Task<T> GetByIdAsync(Guid id);
+
+        Task<T> GetSingleAsync(Expression<Func<bool, T>> predicate);
+
+        Task<IQueryable<T>> GetAllAsync(bool tracking = false);
+    }
+}
