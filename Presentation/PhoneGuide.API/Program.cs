e5445471f72481a0ac13067c2abe8e1533e09b8f
Aykut DEMİRCİ <aykutdemirci@outlook.com>
2024-06-11 06:03:04 +0300

    add seri log

--- Presentation/PhoneGuide.API/Program.cs
+++ Presentation/PhoneGuide.API/Program.cs
@@ -1,6 +1,7 @@
 using PhoneGuide.Infrastructure;
 using PhoneGuide.Infrastructure.Enums;
 using PhoneGuide.Persistance;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,16 @@ builder.Services.AddCache(CachingType.InMemory);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var looger = new LoggerConfiguration()
+.WriteTo.Console()
+.WriteTo.File("logs/log.txt")
+.WriteTo.PostgreSQL(builder.Configuration.GetConnectionString("PostgreSQL"), "Logs", needAutoCreateTable: true)
+.Enrich.FromLogContext()
+.MinimumLevel.Information()
+.CreateLogger();
+
+builder.Host.UseSerilog(looger);
 
 var app = builder.Build();
 
