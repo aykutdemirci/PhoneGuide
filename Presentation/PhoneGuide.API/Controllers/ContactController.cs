2009fa7404846dd5825aab398412241ddf88de34
Aykut DEMİRCİ <aykutdemirci@outlook.com>
2024-06-11 05:27:23 +0300

    contact controller added

---
+++ Presentation/PhoneGuide.API/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneGuide.Application.Abstractions.Services;
+using PhoneGuide.Application.Dto.Contact;
+using System.Net;
+
+namespace PhoneGuide.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(DtoCreateContact dtoCreateContact)
+        {
+            var result = await _contactService.CreateAsync(dtoCreateContact);
+
+            if (result) return new StatusCodeResult((int)HttpStatusCode.Created);
+
+            return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
+        }
+    }
+}
