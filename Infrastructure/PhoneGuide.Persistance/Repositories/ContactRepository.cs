30b7074cda799de805454b2570056afa976286db
Aykut DEMİRCİ <aykutdemirci@outlook.com>
2024-06-11 03:43:44 +0300

    repositories concreted

---
+++ Infrastructure/PhoneGuide.Persistance/Repositories/ContactRepository.cs
@@ -0,0 +1,13 @@
+﻿using PhoneGuide.Application.Repositories;
+using PhoneGuide.Domain.Entities;
+using PhoneGuide.Persistance.Contexts;
+
+namespace PhoneGuide.Persistance.Repositories
+{
+    public class ContactRepository : Repository<Contact>, IContactRepository
+    {
+        public ContactRepository(PhoneGuideDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
---
+++ Infrastructure/PhoneGuide.Persistance/Repositories/PersonRepository.cs
@@ -0,0 +1,13 @@
+﻿using PhoneGuide.Application.Repositories;
+using PhoneGuide.Domain.Entities;
+using PhoneGuide.Persistance.Contexts;
+
+namespace PhoneGuide.Persistance.Repositories
+{
+    public class PersonRepository : Repository<Person>, IPersonRepository
+    {
+        public PersonRepository(PhoneGuideDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
