be868b1320d21611e13bea21a779333d41523a85
Aykut DEMİRCİ <aykutdemirci@outlook.com>
2024-06-11 18:18:13 +0300

    report entity

---
+++ Core/PhoneGuide.Domain/Entities/Enums/ReportStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace PhoneGuide.Domain.Entities.Enums
+{
+    public enum ReportStatus
+    {
+        Preparing,
+
+        Completed
+    }
+}
---
+++ Core/PhoneGuide.Domain/Entities/Report.cs
@@ -0,0 +1,12 @@
+﻿using PhoneGuide.Domain.Entities.Common;
+using PhoneGuide.Domain.Entities.Enums;
+
+namespace PhoneGuide.Domain.Entities
+{
+    public class Report : BaseEntity
+    {
+        public DateTime RequestedDate { get; set; }
+
+        public ReportStatus ReportStatus { get; set; }
+    }
+}
